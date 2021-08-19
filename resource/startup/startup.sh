2dc0bc6aa03ee4dca51c4451ec3e6ac1b1866314
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-19 08:43:20 +0700

    Update startup.sh

---
+++ resource/startup/startup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# SkyNet-Userbot
+# Copyright © 2021 SkyNet-Userbot
+#
+# Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+
+
+echo "
+         ██╗░░░██╗░░░██╗███╗░░██╗██╗░░██╗
+         ██║░░░╚██╗░██╔╝████╗░██║╚██╗██╔╝
+         ██║░░░░╚████╔╝░██╔██╗██║░╚███╔╝░
+         ██║░░░░░╚██╔╝░░██║╚████║░██╔██╗░
+         ███████╗░██║░░░██║░╚███║██╔╝╚██╗
+         ╚══════╝░╚═╝░░░╚═╝░░╚══╝╚═╝░░╚═╝
+       Thank you very much for using SkyNet
+"
+
+python3 -m userbot
