20e1b66412880f965f0a661f7c3aaf2e59f33e75
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-19 11:16:50 +0700

    fixed

--- SkyNet/modules/__init__.py
+++ SkyNet/modules/__init__.py
@@ -1,21 +1,16 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.c (the "License");
-# you may not use this file except in compliance with the License.
-#
 """ Init file which loads all of the modules """
-from userbot import LOGS
+from SkyNet import LOGS
 
 
 def __list_all_modules():
     import glob
     from os.path import basename, dirname, isfile
 
-    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    mod_paths = glob.glob(dirname(__file__) + "/*$py")
     all_modules = [
         basename(f)[:-3]
         for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+        if isfile(f) and f.endswith("$py") and not f.endswith("__init__.py")
     ]
     return all_modules
 
