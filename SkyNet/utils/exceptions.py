b3b93a67d01f29003dd5950923e03b588a43911b
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-19 10:06:13 +0700

    fixed

---
+++ SkyNet/utils/exceptions.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2020 Adek Maulana
+#
+# SPDX-License-Identifier: GNU-3.0-or-later
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class CancelProcess(Exception):
+    """Cancel Process"""
