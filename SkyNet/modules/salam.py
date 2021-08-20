2e439eb20d258b95ea797160933b0e0eef326953
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-08-20 12:12:04 +0700

    fixed

--- SkyNet/modules/salam.py
+++ SkyNet/modules/salam.py
@@ -1,36 +1,59 @@
-from userbot import CMD_HELP
-from userbot.events import register
+from SkyNet import CMD_HELP
+from SkyNet.events import register
 
 
-@register(outgoing=True, pattern='^.P(?: |$)(.*)')
+@register(outgoing=True, pattern='^$P(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`as-salamu'alaikum wr. wb.`🙏")
+    await typew.edit("`As-salamu'alaikum wr. wb.`🙏")
 
 
-@register(outgoing=True, pattern='^.p(?: |$)(.*)')
+@register(outgoing=True, pattern='^$p(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`as-salamu'alaikum wr. wb.`🙏")
+    await typew.edit("`As-salamu'alaikum wr. wb.`🙏")
 
 
-@register(outgoing=True, pattern='^.L(?: |$)(.*)')
+@register(outgoing=True, pattern='^$PA(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`wa'alaikumussalam wr. wb.`🙏")
+    await typew.edit("`As-salamu'alaikum wr. wb. Sayang`🙏")
 
 
-@register(outgoing=True, pattern='^.l(?: |$)(.*)')
+@register(outgoing=True, pattern='^$pa(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`wa'alaikumussalam wr. wb.`🙏")
+    await typew.edit("`As-salamu'alaikum wr. wb. Sayang`🙏")
 
+
+@register(outgoing=True, pattern='^$L(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`Wa'alaikumussalam wr. wb.`🙏")
+
+
+@register(outgoing=True, pattern='^$LA(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`Wa'alaikumussalam wr. wb.`🙏")
+
+
+@register(outgoing=True, pattern='^$l(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`Wa'alaikumussalam wr. wb.`🙏")
+
+
+@register(outgoing=True, pattern='^$la(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("`Wa'alaikumussalam wr. wb. Sayang`🙏")
 
 CMD_HELP.update({
     "salam":
     "✘ Pʟᴜɢɪɴ : Assalamu'alaikum wr. wb.\
-\n\n⚡𝘾𝙈𝘿⚡: `.P`\
+\n\n⚡𝘾𝙈𝘿⚡: `$P`\
 \n↳ : Untuk Memberi Salam.\
-\n\n⚡𝘾𝙈𝘿⚡: `.L`\
+\n\n⚡𝘾𝙈𝘿⚡: `$L`\
 \n↳ : Untuk Menjawab Salam."
 })
